acda02d17d62f3fba0587b75b6e307aba4f797a4
gucheen <guchengf@gmail.com>
2025-12-30 17:44:58 +0800

    feat: update UA

--- src/modules/weibo/api/common.ts
+++ src/modules/weibo/api/common.ts
@@ -1,7 +1,7 @@
 import { AxiosError } from "axios";
 
 export const TIME_OUT = 3000;
-export const MOCK_UA = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Mobile Safari/537.36';
+export const MOCK_UA = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Mobile Safari/537.36';
 
 export const handleForbiddenErr = (err: AxiosError, cb: () => Promise<void>) => {
   if (err.response && [418, 403].includes(err.response.status)) {
